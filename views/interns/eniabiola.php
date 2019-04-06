f81f84baa3ea08686beef5a32d293cf630cc58f8
eniabiola <awosanyaenitan@gmail.com>
2019-04-06 03:34:41 +0100

    This is my profile @eniabiola

---
+++ views/interns/eniabiola.php
@@ -0,0 +1,38 @@
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/eniabiola/image/upload/v1554424679/eniabiola.jpg" alt="eniabiola Awosanya Enitan">
+                        <h6>Awosanya Enitan</h6>
+                        <p>Full-Stack developer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/OnimoEro_Enitan" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/eniabiola" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@engineerenny" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>Hello, I am Awosanya Enitan, a Software developer, lover of PHP, excellent student. I believe all mountains are movable only if the mind has moved them first.
+                        Set your mind free, Set yourself free.</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>JavaScript | PHP | HTML | CSS | MySQL</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
